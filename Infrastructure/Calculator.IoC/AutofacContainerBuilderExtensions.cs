737b1c2f570b0858a3cb61c129872295e2326831
Aaron Urkin <a.urkin@outlook.com>
2021-07-29 07:01:58 +0300

    DI has been prepared

---
+++ Infrastructure/Calculator.IoC/AutofacContainerBuilderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+using Microsoft.Extensions.Configuration;
+
+namespace Calculator.IoC
+{
+    public static class AutofacContainerBuilderExtensions
+    {
+        public static ContainerBuilder AddApplication(this ContainerBuilder builder, IConfiguration configuration)
+        {
+            return builder;
+        }
+    }
+}

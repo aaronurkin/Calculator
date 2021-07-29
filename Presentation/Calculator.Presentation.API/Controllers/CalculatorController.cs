e47b626acbd3f85d13a781518c6745de62f61f41
Aaron Urkin <a.urkin@outlook.com>
2021-07-29 07:08:32 +0300

    API entrypoint has been added

---
+++ Presentation/Calculator.Presentation.API/Controllers/CalculatorController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Calculator.Presentation.API.Controllers
+{
+    [ApiController]//44370
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+    }
+}

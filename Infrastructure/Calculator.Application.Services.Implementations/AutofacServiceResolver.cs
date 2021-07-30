442dc12005215198cb8cc7761394effe4b786c48
Aaron Urkin <a.urkin@outlook.com>
2021-07-30 15:04:01 +0300

    Autofac service resolver has been refactored

--- Infrastructure/Calculator.Application.Services.Implementations/AutofacServiceResolver.cs
+++ Infrastructure/Calculator.Application.Services.Implementations/AutofacServiceResolver.cs
@@ -1,29 +1,59 @@
 ﻿using Autofac;
+using Microsoft.Extensions.Logging;
 
 namespace Calculator.Application.Services.Implementations
 {
     public class AutofacServiceResolver : IServiceResolver
     {
         private readonly ILifetimeScope lifetimeScope;
+        private readonly ILogger<AutofacServiceResolver> logger;
 
-        public AutofacServiceResolver(ILifetimeScope lifetimeScope)
+        public AutofacServiceResolver(
+            ILifetimeScope lifetimeScope,
+            ILogger<AutofacServiceResolver> logger
+        )
         {
+            this.logger = logger;
             this.lifetimeScope = lifetimeScope;
         }
 
         public TService Resolve<TService>()
         {
-            return this.lifetimeScope.Resolve<TService>();
+            try
+            {
+                return this.lifetimeScope.Resolve<TService>();
+            }
+            catch (System.Exception exception)
+            {
+                this.logger.LogError(exception, "FAILED resolving {0}", typeof(TService).FullName);
+                return default(TService);
+            }
         }
 
         public TService ResolveKeyed<TService>(object key)
         {
-            return this.lifetimeScope.ResolveKeyed<TService>(key);
+            try
+            {
+                return this.lifetimeScope.ResolveKeyed<TService>(key);
+            }
+            catch (System.Exception exception)
+            {
+                this.logger.LogError(exception, "FAILED resolving keyed {0}", typeof(TService).FullName);
+                return default(TService);
+            }
         }
 
         public TService ResolveNamed<TService>(string name)
         {
-            return this.lifetimeScope.ResolveNamed<TService>(name);
+            try
+            {
+                return this.lifetimeScope.ResolveNamed<TService>(name);
+            }
+            catch (System.Exception exception)
+            {
+                this.logger.LogError(exception, "FAILED resolving named {0}", typeof(TService).FullName);
+                return default(TService);
+            }
         }
     }
 }

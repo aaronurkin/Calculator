57b1b0b96f4af100674d6d4708448d093d5813d1
Aaron Urkin <a.urkin@gmail.com>
2022-08-21 22:57:40 +0300

    Change request dependent services scope  to request

--- nestjs8.2.5/src/providers/multiplication-operation.provider.ts
+++ nestjs8.2.5/src/providers/multiplication-operation.provider.ts
@@ -1,9 +1,10 @@
-import { Provider } from '@nestjs/common';
+import { Provider, Scope } from '@nestjs/common';
 import { IOperation } from '@services/application/operation';
 import { ServiceToken } from '@services/application/service-token';
 import { MultiplicationOperation } from '@infrastructure/application/operations/multiplication.operation';
 
 export const MultiplicationOperationProvider: Provider<IOperation> = {
+  scope: Scope.REQUEST,
   provide: `${ServiceToken.operation}*`,
   useClass: MultiplicationOperation,
 };
--- nestjs8.2.5/src/providers/subtraction-operation.provider.ts
+++ nestjs8.2.5/src/providers/subtraction-operation.provider.ts
@@ -1,9 +1,10 @@
-import { Provider } from '@nestjs/common';
+import { Provider, Scope } from '@nestjs/common';
 import { IOperation } from '@services/application/operation';
 import { ServiceToken } from '@services/application/service-token';
 import { SubtractionOperation } from '@infrastructure/application/operations/subtraction.operation';
 
 export const SubtractionOperationProvider: Provider<IOperation> = {
+  scope: Scope.REQUEST,
   provide: `${ServiceToken.operation}-`,
   useClass: SubtractionOperation,
 };
